89e6d641c968c49f62fbd06aee1ee73858655e15
Mykolas Mankevicius <mykolas.mankevicius@g2ocean.com>
2023-03-31 05:50:44 +0300

    Lesson 1: Write React with Javascript

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// 1. Import dependencies
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// 2. Create a React element
+const element = React.createElement(
+  "h1",
+  {
+    id: "hello",
+    class: "hello",
+    style: { background: "red" },
+    "wai-aria": "hey",
+  },
+  "Hello World!"
+);
+
+// 3. Render the application
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(element);
